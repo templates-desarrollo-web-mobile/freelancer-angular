d714205245ea2c91a8255a6dc333e0b9441aa33e
Anartz Mugika Ledo <mugan86@gmail.com>
2020-02-11 19:38:52 +0100

    Adaptado el modal

--- src/app/@components/portfolio/portfolio.component.ts
+++ src/app/@components/portfolio/portfolio.component.ts
@@ -11,12 +11,12 @@ export class PortfolioComponent implements OnInit {
 
   items: Array<IModal> = [];
   ngOnInit() {
-    this.items.push({title: 'Log Cabin', description: '', image: 'assets/img/portfolio/cabin.png'});
-    this.items.push({title: 'Tasty Cake', description: '', image: 'assets/img/portfolio/cake.png'});
-    this.items.push({title: 'Circus Tent', description: '', image: 'assets/img/portfolio/circus.png'});
-    this.items.push({title: 'Controller', description: '', image: 'assets/img/portfolio/game.png'});
-    this.items.push({title: 'Locked Safe', description: '', image: 'assets/img/portfolio/safe.png'});
-    this.items.push({title: 'Submarine', description: '', image: 'assets/img/portfolio/submarine.png'});
+    this.items.push({title: 'Log Cabin', description: 'rrrr', image: 'assets/img/portfolio/cabin.png'});
+    this.items.push({title: 'Tasty Cake', description: 'rrr', image: 'assets/img/portfolio/cake.png'});
+    this.items.push({title: 'Circus Tent', description: 'rrr', image: 'assets/img/portfolio/circus.png'});
+    this.items.push({title: 'Controller', description: 'tttt', image: 'assets/img/portfolio/game.png'});
+    this.items.push({title: 'Locked Safe', description: 'ttt', image: 'assets/img/portfolio/safe.png'});
+    this.items.push({title: 'Submarine', description: 'ttt', image: 'assets/img/portfolio/submarine.png'});
   }
 
 }
--- src/app/@widgets/image-item/image-item.module.ts
+++ src/app/@widgets/image-item/image-item.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ImageItemComponent } from './image-item.component';
 
-
-
 @NgModule({
   declarations: [ImageItemComponent],
   imports: [
