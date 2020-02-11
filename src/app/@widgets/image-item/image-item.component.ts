c67ecea04779d988fc2b2fee8ae3d0b185086079
Anartz Mugika Ledo <mugan86@gmail.com>
2020-02-11 09:08:14 +0100

    Preparado componente de la imagen para cargarlo individualmente con cada item

--- src/app/@widgets/image-item/image-item.component.ts
+++ src/app/@widgets/image-item/image-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ModalService } from '@widgets/modal/modal.service';
 import { IModal } from '@widgets/modal/modal.interface';
 
@@ -9,6 +9,7 @@ import { IModal } from '@widgets/modal/modal.interface';
 })
 export class ImageItemComponent implements OnInit {
 
+  @Input() info: IModal;
   constructor(private modal: ModalService) { }
 
   ngOnInit() {
