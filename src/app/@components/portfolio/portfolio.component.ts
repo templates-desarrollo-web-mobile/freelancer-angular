746fbbb365cce913c85f57477133daeaf684e270
Anartz Mugika Ledo <mugan86@gmail.com>
2020-02-11 09:17:56 +0100

    Añadido modulo image item para usarlo dentro de portfolio y añadir la infor de los items del portfolio

--- src/app/@components/portfolio/portfolio.component.ts
+++ src/app/@components/portfolio/portfolio.component.ts
@@ -13,12 +13,12 @@ export class PortfolioComponent implements OnInit {
   constructor(private modal: ModalService) { }
 
   ngOnInit() {
-    this.items.push({title: 'Log Cabin', description: '', image: ''});
-    this.items.push({title: '', description: '', image: ''});
-    this.items.push({title: '', description: '', image: ''});
-    this.items.push({title: '', description: '', image: ''});
-    this.items.push({title: '', description: '', image: ''});
-    this.items.push({title: '', description: '', image: ''});
+    this.items.push({title: 'Log Cabin', description: '', image: 'assets/img/portfolio/cabin.png'});
+    this.items.push({title: 'Tasty Cake', description: '', image: 'assets/img/portfolio/cake.png'});
+    this.items.push({title: 'Circus Tent', description: '', image: 'assets/img/portfolio/circus.png'});
+    this.items.push({title: 'Controller', description: '', image: 'assets/img/portfolio/game.png'});
+    this.items.push({title: 'Locked Safe', description: '', image: 'assets/img/portfolio/safe.png'});
+    this.items.push({title: 'Submarine', description: '', image: 'assets/img/portfolio/submarine.png'});
   }
 
   open() {
