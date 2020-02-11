6bc49f7b9dcd862989c6bd5812dd1b796a84b5dd
Anartz Mugika Ledo <mugan86@gmail.com>
2020-02-11 09:12:57 +0100

    Empezar adaptando los items del portfolio

--- src/app/@components/portfolio/portfolio.component.ts
+++ src/app/@components/portfolio/portfolio.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalService } from '@widgets/modal/modal.service';
+import { IModal } from '@widgets/modal/modal.interface';
 
 @Component({
   selector: 'app-portfolio',
@@ -8,9 +9,16 @@ import { ModalService } from '@widgets/modal/modal.service';
 })
 export class PortfolioComponent implements OnInit {
 
+  items: Array<IModal> = [];
   constructor(private modal: ModalService) { }
 
   ngOnInit() {
+    this.items.push({title: 'Log Cabin', description: '', image: ''});
+    this.items.push({title: '', description: '', image: ''});
+    this.items.push({title: '', description: '', image: ''});
+    this.items.push({title: '', description: '', image: ''});
+    this.items.push({title: '', description: '', image: ''});
+    this.items.push({title: '', description: '', image: ''});
   }
 
   open() {
