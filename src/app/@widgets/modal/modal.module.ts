ee84f4c1c916c6deb9378611f580f1e9ef3bb36f
Anartz Mugika Ledo <mugan86@gmail.com>
2020-02-13 19:14:10 +0100

    Terminada clase 3

--- src/app/@widgets/modal/modal.module.ts
+++ src/app/@widgets/modal/modal.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal.component';
+import { TitleModule } from '@widgets/title/title.module';
 
 @NgModule({
   declarations: [ModalComponent],
   imports: [
     CommonModule
   ],
-  exports: [ModalComponent],
+  exports: [ModalComponent]
 })
 export class ModalModule { }
