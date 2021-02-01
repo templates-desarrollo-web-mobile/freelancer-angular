3bd9eb1915e7e3c959a3573c8c0e97ef5bc70ad3
Anartz Mugika Ledo <mugan86@gmail.com>
2021-02-01 08:32:56 +0100

    Update navbar.component.ts

--- src/app/@components/navbar/navbar.component.ts
+++ src/app/@components/navbar/navbar.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  menuOptionsActive: Array<boolean> = [
+    true,
+    false,
+    false
+  ];
   constructor() { }
 
   ngOnInit() {
+  }
+
+  select(itemPos: number) {
+    for (let i = 0; i <= 3; i++) {
+      this.menuOptionsActive[i] = (i === itemPos) ? true : false;
+    }
   }
 
 }
